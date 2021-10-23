82c7d5dd124a63bebf64def594259db8ef5dcd49
Tomyx2811 <tomasdgomezg@gmail.com>
2021-10-22 19:10:42 -0500

    primera pagina

---
+++ JS/Formulario.js
@@ -0,0 +1,25 @@
+alert ("En esta pagina web se encuentra contenido sobre Videojugos en computador")
+
+let edad = 13
+
+if  (edad>7) {
+    alert ("Diviertete en esta pagina web")
+}
+else {
+    alert ("No tienes la edad suficiente para entrar a esta pagina, losiento")
+}
+
+
+function sumar (numero1,numero2) {
+              5    +    6
+    return numero1 + numero2
+}
+
+sumar (5,6) = 11
+
+function saludar (nombre, apellido, edad) {
+    alert("Hola" + nombre + apellido + "tu tienes" + edad)
+}
+
+saludar ("tomas", "gomez", 13)
+
